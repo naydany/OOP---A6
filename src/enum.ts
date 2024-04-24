460eca8dbf8c9f9c38296d0eccdf29781f8ae071
unknown <vanny.mao@student.passerellesnumeriques.org>
2024-04-24 10:58:06 +0700

    [fea] add meal to booking OPA-7

--- src/enum.ts
+++ src/enum.ts
@@ -1,5 +1,5 @@
 // job category
-export enum JobCategory{
+export enum JobCategory {
     admin,
     crew,
     pilot,
@@ -12,27 +12,31 @@ export enum JobCategory{
 
 //booking status
 export enum BookingStatus {
-    PENDING='PENDING',
+    PENDING = 'PENDING',
     CONFIRMED = 'CONFIRMED',
-    CANCELLED= 'CANCELLED'
+    CANCELLED = 'CANCELLED'
 }
+
+
 // gender
-export enum Gender{
+export enum Gender {
     Male,
     Female
 }
 
+
 //meal type
-export enum Meal{
-    Vegetarian,
-    Vegan,
-    Dairy_free,
-    Halal_Kosher,
-    Forth
+export enum Meal {
+    Vegetarian = "Vegetarian",
+    Vegan = "Vegan",
+    Dairy_free = "Dairy_free",
+    Halal_Kosher = "Halal_K",
+    Forth = "Forth",
 }
 
+
 //Seat type
-export enum SeatType{
+export enum SeatType {
     Business_classic,
     Business_flex,
     Business_class
