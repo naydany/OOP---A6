77961004ec47f70b9d1daa7d5c6e91dd155afc17
naydany <dany.nay@student.passerellesnumeriques.org>
2024-04-28 11:52:05 +0700

    [imp] update_seat OPA-10

--- src/ticket.ts
+++ src/ticket.ts
@@ -2,18 +2,19 @@
 import { Flight } from "./flight";
 import { Passenger } from "./passenger";
 import { SeatType} from "./enum";
+import { Seats } from "./seats";
 
 export class Ticket {
     private ticketType: SeatType;
     // private passenger: Passenger;
     private flight: Flight;
+    private seat:Seats;
 
-    constructor(ticketType: SeatType,flight: Flight) {
+    constructor(ticketType: SeatType,flight: Flight,seat:Seats) {
         this.ticketType = ticketType;
         // this.passenger = passenger;
         this.flight = flight;
+        this.seat =seat;
     }
-
-    
 
 }
