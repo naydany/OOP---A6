9b5d27efff5e4597f2c31cfef9490c6b31bda610
RinYouern <rin.youern@student.passerellesnumeriques.org>
2024-04-29 09:23:11 +0700

    [feat] cleate class flight OPA-24

--- src/flight.ts
+++ src/flight.ts
@@ -1,7 +1,18 @@
+// import from other 
+import { DateTime } from "./dateTime";
+
 export class Flight{
     private flightNumber:string;
-    constructor(flightNumber:string){
+    private departure:DateTime;
+    private arrival:DateTime;
+    private origin:string;
+    private destination:string;
+    constructor(flightNumber:string,departure:DateTime,arrival:DateTime,origin:string,destination:string){
         this.flightNumber=flightNumber;
+        this.departure=departure;
+        this.arrival=arrival;
+        this.origin=origin;
+        this.destination=destination;
     }
 
     //method
